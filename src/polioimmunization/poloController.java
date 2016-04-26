d99734ae5da568a9bdfb054090824b48298b618a
Mira Rosie Jacobs <mirajacobs777@gmail.com>
2016-04-26 14:39:46 -0500

    Java Project items

--- src/polioimmunization/poloController.java
+++ src/polioimmunization/poloController.java
@@ -8,13 +8,19 @@ package polioimmunization;
 import java.net.*;
 import java.util.*;
 import com.google.gson.*;
+import java.awt.MenuItem;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Slider;
 
 
 /**
@@ -26,12 +32,51 @@ public class poloController implements Initializable {
     @FXML
     private BarChart bar;
     
+    @FXML 
+    private Slider minimum; 
+    
+    @FXML
+    private Slider maximum; 
+    
+    private Dataset data;
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("You clicked me!");  
+    }
+    
+    @FXML
+    private void close(){
+        System.exit(0); 
         
     }
     
+    @FXML
+    private void about(){
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle("About");
+    alert.setHeaderText("Polio Immunization");
+    alert.setContentText(" This data looks at the percentages of Polio Immunizations by Country");
+
+    alert.showAndWait();
+    
+    }
+    
+    @FXML
+    private void handleChangeFilter(Event event) {
+        System.out.println("Clicked slider");
+        XYChart.Series< String, Number> ImmunizationSeries = new XYChart.Series();
+        Datapoint[] point= data.getFact(); 
+        
+        for(Datapoint xyz : point){
+            if(xyz.getDim().getCountry() != null){
+                if(xyz.getValue() > minimum.getValue() && xyz.getValue() < maximum.getValue()){
+                    ImmunizationSeries.getData().add(new XYChart.Data(xyz.getDim().getCountry(), xyz.getValue()));     
+                }
+            }
+        }
+        bar.getData().setAll(ImmunizationSeries);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         String s = "http://apps.who.int/gho/athena/data/GHO/WHS4_544.json?profile=simple&filter=YEAR:1980";
@@ -61,15 +106,14 @@ public class poloController implements Initializable {
         scan.close();
 
         Gson gson = new Gson();
-        Dataset data = gson.fromJson(str, Dataset.class);
+        data = gson.fromJson(str, Dataset.class);
         System.out.println(data.toString());
 
         XYChart.Series< String, Number> ImmunizationSeries = new XYChart.Series();
         Datapoint[] point= data.getFact(); 
         for(Datapoint xyz : point){
             if(xyz.getDim().getCountry() != null){
-                ImmunizationSeries.getData().add(new XYChart.Data(xyz.getDim().getCountry(), xyz.getValue()));      
-
+                ImmunizationSeries.getData().add(new XYChart.Data(xyz.getDim().getCountry(), xyz.getValue()));     
         }
         }
         bar.getData().add(ImmunizationSeries);
